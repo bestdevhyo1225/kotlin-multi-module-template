a388cd35feb956c27f56dcdbad2d806464c12ee6
janghyoseog <bestdevhyo1225@gmail.com>
2023-06-27 20:51:22 +0900

    refactor: 비밀번호 유효성 검증에서 @NotBlank 제거

--- application/api/src/main/kotlin/kr/co/hyo/api/member/controller/request/MemberChangePasswordRequest.kt
+++ application/api/src/main/kotlin/kr/co/hyo/api/member/controller/request/MemberChangePasswordRequest.kt
@@ -1,16 +1,13 @@
 package kr.co.hyo.api.member.controller.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
 import kr.co.hyo.common.util.validator.Password
 
 data class MemberChangePasswordRequest(
-    @field:NotBlank(message = "기존 비밀번호를 입력하세요")
     @field:Password
     @field:Schema(description = "기존 비밀번호", example = "!@zdx90b8Cl3x2%&dz", required = true)
     val oldPassword: String,
 
-    @field:NotBlank(message = "변경할 비밀번호를 입력하세요")
     @field:Password
     @field:Schema(description = "변경할 비밀번호", example = "!@3d#Lew9%23Kz4kaC", required = true)
     val newPassword: String,
