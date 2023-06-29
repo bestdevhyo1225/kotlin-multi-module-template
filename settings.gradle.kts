342bfb7c9ef9eefc57aea4aa100f0a9124198c1a
janghyoseog <bestdevhyo1225@gmail.com>
2023-06-29 18:49:35 +0900

    chore: api:kafka-consumer, client:kafka-publisher 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ rootProject.name = "kotlin-multi-module-template"
 include(
     "application:api",
     "application:batch",
+    "application:kafka-consumer",
+    "client:kafka-publisher",
     "common",
     "domain:nosql:redis",
     "domain:rdbms:jpa",
