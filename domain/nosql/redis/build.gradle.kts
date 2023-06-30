846543e5bf9d466e410d4a48c28500678f891ef7
janghyoseog <bestdevhyo1225@gmail.com>
2023-06-30 14:23:17 +0900

    chore: 취약성을 해결하기 위한 라이브러리 최신 버전으로 업데이트

--- domain/nosql/redis/build.gradle.kts
+++ domain/nosql/redis/build.gradle.kts
@@ -14,4 +14,10 @@ dependencies {
         // 위의 예외가 발생하지 않도록 slf4j-simple 모듈을 제외한다.
         exclude("org.slf4j", "slf4j-simple")
     }
+    // Embedded Redis 라이브러리 사용시, 해결하기 위한 com.google.guava 의존성 수정
+    // CWE-770 - Allocation of Resources Without Limits or Throttling
+    implementation("com.google.guava:guava:32.0.1-jre")
+    // Embedded Redis 라이브러리 사용시, 해결하기 위한 common-ios:commons-io 의존성 수정
+    // 4.8 Improper Limitation of a Pathname to a Restricted Directory ('Path Traversal') vulnerability pending CVSS allocation
+    implementation("commons-io:commons-io:2.13.0")
 }
