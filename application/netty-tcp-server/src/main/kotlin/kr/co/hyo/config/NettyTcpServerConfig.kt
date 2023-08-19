2b5a8f3b64a57e9d587e408c7fb1e1c5b507fc87
janghyoseog <bestdevhyo1225@gmail.com>
2023-08-19 16:02:54 +0900

    chore: WorkerGroup의 count를 지정하지 않고, default 값이 사용되도록 수정

--- application/netty-tcp-server/src/main/kotlin/kr/co/hyo/config/NettyTcpServerConfig.kt
+++ application/netty-tcp-server/src/main/kotlin/kr/co/hyo/config/NettyTcpServerConfig.kt
@@ -24,9 +24,6 @@ class NettyTcpServerConfig(
     @Value("\${server.netty.boss-count}")
     private val bossCount: Int,
 
-    @Value("\${server.netty.worker-count}")
-    private val workerCount: Int,
-
     @Value("\${server.netty.keep-alive}")
     private val keepAlive: Boolean,
 
@@ -57,7 +54,11 @@ class NettyTcpServerConfig(
 
     // worker: boss가 수락한 연결의 트래픽 관리
     @Bean(destroyMethod = "shutdownGracefully")
-    fun nettyTcpServerWorkerGroup(): NioEventLoopGroup = NioEventLoopGroup(workerCount)
+    fun nettyTcpServerWorkerGroup(): NioEventLoopGroup =
+        // workerCount를 지정하지 않으면 default 값 사용 (Worker 스레드 풀의 default 값 = CPU 코어 * 2)
+        // DEFAULT_EVENT_LOOP_THREADS = Math.max(1, SystemPropertyUtil.getInt(
+        //                "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2))
+        NioEventLoopGroup()
 
     // IP 소켓 주소(IP 주소, Port 번호)를 구현
     // 도메인 이름으로 객체 생성 가능
