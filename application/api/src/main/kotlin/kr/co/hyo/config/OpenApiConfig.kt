0aa42e4825980012f9bd4b50eabf1eafb1b37ae2
janghyoseog <bestdevhyo1225@gmail.com>
2023-10-07 17:40:16 +0900

    chore: OpenApiConfig에서 test profile 제외

--- application/api/src/main/kotlin/kr/co/hyo/config/OpenApiConfig.kt
+++ application/api/src/main/kotlin/kr/co/hyo/config/OpenApiConfig.kt
@@ -9,8 +9,10 @@ import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile(value = ["!test"])
 class OpenApiConfig(
     @Value("\${open-api.service.gateway.url}")
     private val gatewayUrl: String,
