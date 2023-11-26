fb325827cf1cba8b809c8fd77a2bae3598a8e4ca
janghyoseog <bestdevhyo1225@gmail.com>
2023-11-26 20:35:36 +0900

    spring-boot-starter-aop, caffeine, bucket4j 라이브러리 추가

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,4 +10,7 @@ dependencies {
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("org.bitbucket.b_c:jose4j:0.9.3")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+//    implementation("com.bucket4j:bucket4j-core:8.7.0")
 }
